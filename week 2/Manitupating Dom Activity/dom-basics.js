55af517bb4ce2902126480a8d2f75af043201672
marcosamos <ucs21001@byui.edu>
2022-01-10 14:55:44 -0800

    cahnges

---
+++ week 2/Manitupating Dom Activity/dom-basics.js
@@ -0,0 +1,16 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+const newImage = document.createElement("img");
+newImage.setAttribute('src', 'https://placeimg.com/200/200/animals')
+newImage.setAttribute("alt","Some animal")
+document.body.appendChild(newImage);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+const newSection = document.createElement("section");
+newSection.innerHTML = "<h2>Cse 12b</h2><p>Welcome to Javascript Language</p>";
+document.body.appendChild(newSection);
---
+++ week 2/Prove tast/scripts/task2.js
@@ -0,0 +1,58 @@
+/* Lesson 2 */
+
+/* VARIABLES */
+
+// Step 1: declare and instantiate a variable to hold your name
+var myName = "Marcos"
+
+// Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
+document.getElementById("name").innerHTML = myName
+
+// Step 3: declare and instantiate a variable to hold the current year
+var year = 2022
+
+// Step 4: place the value of the current year variable into the HTML file
+document.getElementById("year").innerHTML = year
+
+// Step 5: declare and instantiate a variable to hold the name of your picture
+
+newImage.setAttribute('src', '/Prove tast/images/IMG-20210422-WA0012.jpg')
+newImage.setAttribute("alt","Me")
+document.querySelector("img").src = '/Prove tast/images/IMG-20210422-WA0012.jpg';
+// Step 6: copy your image into the "images" folder
+
+// Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
+
+
+
+
+/* ARRAYS */
+
+// Step 1: declare and instantiate an array variable to hold your favorite foods
+
+
+// Step 2: place the values of the favorite foods variable into the HTML file
+
+
+// Step 3: declare and instantiate a variable to hold another favorite food
+
+
+// Step 4: add the variable holding another favorite food to the favorite food array
+
+
+// Step 5: repeat Step 2
+
+
+// Step 6: remove the first element in the favorite foods array
+
+
+// Step 7: repeat Step 2
+
+
+// Step 8: remove the last element in the favorite foods array
+
+
+// Step 7: repeat Step 2
+
+
+
